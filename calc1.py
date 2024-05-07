3adc8af6cc0498d338541a6133e2a35a062f2abb
Yukun J <yukunj.cs@gmail.com>
2024-05-06 22:00:29 -0400

    Add exercise annotation

--- calc1.py
+++ calc1.py
@@ -1,3 +1,11 @@
+"""
+1. Modify the code to allow multiple-digit integers in the input, for example “12+3”
+2. Add a method that skips whitespace characters 
+   so that your calculator can handle inputs with whitespace characters like ” 12 + 3”
+3. Modify the code and instead of ‘+’ handle ‘-‘ to evaluate subtractions like “7-5”
+"""
+
+
 # Token types
 #
 # EOF (end-of-file) token is used to indicate that
